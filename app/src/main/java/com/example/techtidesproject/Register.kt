336056c936217aea2d681b92d4405c74d7b28f9e
Mary-Njiru <marynwanjiruu@gmail.com>
2024-07-08 06:57:40 +0300

    adds changes

--- app/src/main/java/com/example/techtidesproject/Register.kt
+++ app/src/main/java/com/example/techtidesproject/Register.kt
@@ -7,18 +7,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.techtidesproject.databinding.ActivityMainBinding
+import com.example.techtidesproject.databinding.ActivityRegisterBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class Register : AppCompatActivity() {
+
+    lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnSignUp.setOnClickListener {
-            val intent = Intent(this,Register::class.java)
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
             startActivity(intent)
-        }
 
+
+        }
     }
 }
